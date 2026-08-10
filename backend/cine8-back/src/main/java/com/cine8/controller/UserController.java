745c7cb6db41456844247193f6faaec952a96f32
Luisma34 <luisma.cordon@gmail.com>
2026-08-10 20:23:35 +0100

    UserController modified

--- backend/cine8-back/src/main/java/com/cine8/controller/UserController.java
+++ backend/cine8-back/src/main/java/com/cine8/controller/UserController.java
@@ -1,17 +1,71 @@
 package com.cine8.controller;
 
+import com.cine8.dto.ChangePasswordDTO;
+import com.cine8.dto.UpdateUserDTO;
+import com.cine8.entity.User;
 import com.cine8.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<User> findByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(userService.findByEmail(email));
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteMyAccount() {
+        userService.deleteByEmail();
+        return ResponseEntity.noContent().build(); // 204
+    }
+
+    @PutMapping("/me/password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDTO changePasswordDTO) {
+
+        userService.changePassword(changePasswordDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/me/update")
+    public ResponseEntity<User> changeMyAccount(@RequestBody UpdateUserDTO updateUserDTO) {
+
+        User userUpdate = userService.updateUser(updateUserDTO);
+
+        return ResponseEntity.ok().body(userUpdate);
+    }
 
 
 }
